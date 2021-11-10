e32e7fb6a40a923f12c64fca670dad3a51885056
Jen Stewart <jenstewart2404@gmail.com>
2021-11-10 21:16:33 +0000

    initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import "../styles/App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -13,8 +11,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
