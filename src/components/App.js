9436e63c2902db0ac33ba820c5c1a16bee02539f
Jen Stewart <jenstewart2404@gmail.com>
2021-11-10 21:36:45 +0000

    copied over basic map functionality with custom overlay, zoom, markers and pins

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,51 @@
 import "../styles/App.css";
+import { MapContainer, ImageOverlay, Marker, Popup } from "react-leaflet";
+import { CRS } from "leaflet";
+import arcanistmiddlelanoscea from "../data/arcanist-middlelanoscea.json";
+import huntIcon from "../helpers/huntIcon.js";
 
 function App() {
+  console.log(arcanistmiddlelanoscea);
+  const toLatLong = ([x, y]) => [42 - y, x];
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <MapContainer
+        bounds={[
+          [1, 1],
+          [42, 42],
+        ]}
+        maxBounds={[
+          [1, 1],
+          [42, 42],
+        ]}
+        attributionControl={false}
+        crs={CRS.Simple}
+        zoomSnap={0}
+        minZoom={4}
+        maxZoom={7}>
+        <ImageOverlay
+          bounds={[
+            [1, 1],
+            [42, 42],
+          ]}
+          url="https://xivapi.com/m/s1f1/s1f1.00.jpg"
+        />
+
+        {arcanistmiddlelanoscea.map((arc) => (
+          <Marker
+            key={arc.id}
+            position={toLatLong([arc.x, arc.y])}
+            icon={huntIcon}>
+            <Popup>
+              <img src={arc.mobIcon} alt={arc.mobName} />
+              <p>{arc.mobName}</p>
+              <p>
+                X: {arc.x}, Y: {arc.y}
+              </p>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 }
---
+++ src/helpers/huntIcon.js
@@ -0,0 +1,11 @@
+import L from "leaflet";
+
+const huntIcon = new L.Icon({
+  iconUrl: "http://xivapi.com/i/060000/060002_hr1.png",
+  iconRetinaUrl: "http://xivapi.com/i/060000/060002_hr1.png",
+  popupAnchor: [0, 0],
+  iconSize: new L.Point(32, 32),
+  className: "huntIcon",
+});
+
+export default huntIcon;
