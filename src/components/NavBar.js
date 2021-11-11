229121aceae37d1fcaa416126d045e93403c4281
Jen Stewart <jenstewart2404@gmail.com>
2021-11-11 15:11:58 +0000

    created navbar, sidebars and basic grid styling

---
+++ src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/navBar.css";
+
+const NavBar = () => {
+  return (
+    <div className="navBar-container">
+      <p>FFXIV Hunting Log</p>
+      Login / Logout / Register
+    </div>
+  );
+};
+
+export default NavBar;
