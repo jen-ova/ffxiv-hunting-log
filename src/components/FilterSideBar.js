10f1e9d3f78844790b722f7da4b314cb64f770ce
Tom Ramsden <thomas.ramsden20@gmail.com>
2021-11-29 22:04:28 +0000

    CSS grid adjustments and some media queries implemented

--- src/components/FilterSideBar.js
+++ src/components/FilterSideBar.js
@@ -11,47 +11,47 @@ const FilterSideBar = () => {
 
   return (
     <Wrapper>
-      <div className="filterSideBar-container">
-        <div className="filterSideBar__job">
-          <p className="filterSideBar__job-title">Class</p>
-          <div className="filterSideBar__job_container">
-          {constants.jobs.map((job, index, e) => (
-            <div className="filterSideBar__job-item" key={job}>
-              <input
-                type="checkbox"
-                value={job}
-                id={job}
-                checked={checkedState[index]}
-                onChange={() => {
-                  handleChange(job);
-                }}
-              />
-              <label htmlFor={job}>{job}</label>
+        <div className="filterSideBar-container">
+          <div className="filterSideBar__job">
+            <p className="filterSideBar__job-title">Class</p>
+            <div className="filterSideBar__job_container">
+            {constants.jobs.map((job, index, e) => (
+              <div className="filterSideBar__job-item" key={job}>
+                <input
+                  type="checkbox"
+                  value={job}
+                  id={job}
+                  checked={checkedState[index]}
+                  onChange={() => {
+                    handleChange(job);
+                  }}
+                />
+                <label htmlFor={job}>{job}</label>
+              </div>
+            ))}
             </div>
-          ))}
+          </div>
+          <div className="filterSideBar__region">
+            <label htmlFor="region" className="filterSideBar__region-title">Region</label>
+            <select id="imageUrl" onChange={handleImageUrl}>
+              {mapImages.map((mapImage) => (
+                <option value={mapImage.slug} key={`option-${mapImage.slug}`}>
+                  {mapImage.title}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className="filterSideBar__rank">
+            <label htmlFor="rank" className="filterSideBar__rank-title">Rank</label>
+            <select name="rank" id="rank" onChange={handleRank}>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
           </div>
         </div>
-        <div className="filterSideBar__region">
-          <label htmlFor="region" className="filterSideBar__region-title">Region</label>
-          <select id="imageUrl" onChange={handleImageUrl}>
-            {mapImages.map((mapImage) => (
-              <option value={mapImage.slug} key={`option-${mapImage.slug}`}>
-                {mapImage.title}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div className="filterSideBar__rank">
-          <label htmlFor="rank" className="filterSideBar__rank-title">Rank</label>
-          <select name="rank" id="rank" onChange={handleRank}>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-        </div>
-      </div>
     </Wrapper>
   );
 };
