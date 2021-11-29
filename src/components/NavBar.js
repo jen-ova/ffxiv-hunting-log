d9eab962e49dbbf0e8cbe43d7c13be20ccaa0232
Tom Ramsden <thomas.ramsden20@gmail.com>
2021-11-29 19:06:25 +0000

    Cleaned up the filter dropdowns

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,8 +9,8 @@ const NavBar = () => {
         <h1 className="navBar-container__title">FFXIV Hunting Log</h1>
         <div className="navBar-container__account">
           <i className="fas fa-filter"></i>
-          <i className="fas fa-bullseye"></i>
-          <i className="fas fa-user"></i>
+          {/* <i className="fas fa-bullseye"></i>
+          <i className="fas fa-user"></i> */}
         </div>
       </div>
     </Wrapper>
